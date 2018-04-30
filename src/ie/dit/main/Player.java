76fba18cbe76411cd48be90b0d659190d39b2eff
Adam Noone <you@example.com>
2018-04-30 21:17:28 +0100

    dinner

--- src/ie/dit/main/Player.java
+++ src/ie/dit/main/Player.java
@@ -9,33 +9,33 @@ import java.util.Random;
 
 public class Player extends GameObject {
 	
-	Random r = new Random();
+	Random r = new Random(); 
 	Handler handler;
 
-	public Player(int x, int y, ID id,Handler handler) {
+	public Player(int x, int y, ID id,Handler handler) {//int vars to be passed into player class {
 		super(x, y, id);
-		this.handler = handler;
+		this.handler = handler; 
 		
 		
 	}
 	
 	public Rectangle getBounds()
 	{
-		return new Rectangle((int)x,(int)y,32,32);
+		return new Rectangle((int)x,(int)y,32,32); //used to detect collision with enemies 
 		
 	}
 
 	
 	public void tick() {
-	x+= velX;
-	y+= velY;
+	x+= velX; // velocity in x axis will be changing based on what key the p[layer presses so we must constantly update it 
+	y+= velY; // velocity in y axis  will be changing based on what key the p[layer presses so we must constantly update it 
 	
-    x = Game.clamp(x,0, Game.WIDTH -47);
-    y = Game.clamp(y,0, Game.HEIGHT -70);
+    x = Game.clamp(x,0, Game.WIDTH -47); //cant go outside game window 
+    y = Game.clamp(y,0, Game.HEIGHT -70);//cant go outside game window 
     
-   //handler.addObject(new BasicEnemyTrail(x,y,ID.BasicEnemyTrail,Color.white,32,32,0.2f,handler));
+   
     
-    collision();
+    collision();//call collision for the player 
 	}
 
 	
@@ -43,14 +43,14 @@ public class Player extends GameObject {
 	{
 		for(int i = 0 ; i < handler.object.size();i++)
 		{
-			GameObject tempObject = handler.object.get(i);
+			GameObject tempObject = handler.object.get(i); //get the object that hit the player 
 			
 			
-		if (tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy|| tempObject.getId() == ID.SmartEnemy) //tempObject is basic enemy
+		if (tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy|| tempObject.getId() == ID.SmartEnemy) //tempObject is an enemy
 			{
-				if (getBounds().intersects(tempObject.getBounds()))//if basicEnemy hits player do the following
+				if (getBounds().intersects(tempObject.getBounds()))//if any of the Enemies hits player do the following
 				{
-					HUD.HEALTH -=2;
+					HUD.HEALTH -=2; //the players health will decrease by 2 
 				}
 			}
 		}
@@ -58,13 +58,9 @@ public class Player extends GameObject {
 
 	public void render(Graphics g) {
 		
-		//graphics 2d has extra methods 
-		//Graphics2D g2d = (Graphics2D) g; //creates a graphics g method and casts it into a graphics 2d variable
+		
+		//draw a white player 
 		g.setColor(Color.white); 
-		
-		
-		//else if (id == ID.Player2)g.setColor(Color.blue); 
-		//g.setColor(Color.white);
 		g.fillRect((int)x,(int) y, 40, 40);
 	}
 
